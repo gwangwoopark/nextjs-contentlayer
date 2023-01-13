838b997202a00cd3a5bb37a7d6f0a650dd4433f2
Gwangwoo Park <gwangwoo.park@gmail.com>
2023-01-13 14:15:19 +0900

    Next.js with TailwindCSS

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center bg-pink-200">
+        <span className="font-bold text-xl animate-bounce">nyfi</span>
+      </div>
+    </>
+  );
+}
